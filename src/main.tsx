fbd116320beb1f2f65ce13916a2f7abbe954dc54
Felipe Suarez <Cristiansabogal51@gmail.com>
2026-07-22 17:07:44 -0500

    tarea - palabras revueltas

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 // import { PokemonPage } from "./03-examples/PokemoPage";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 import { TasksApp } from "./05-useReducer/TaskApp";
+import { ScrambleWords } from "./05-useReducer/ScrambleWords";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 
 createRoot(document.getElementById("root")!).render(
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 );
