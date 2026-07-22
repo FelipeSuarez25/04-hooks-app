3e5f0dfab573222ce8a62fd420e424089c0b659b
Felipe Suarez <Cristiansabogal51@gmail.com>
2026-07-22 15:35:01 -0500

    validadores localstorage con zod

--- src/05-useReducer/TaskApp.tsx
+++ src/05-useReducer/TaskApp.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 
 import { Plus, Trash2, Check } from "lucide-react";
 
@@ -8,17 +8,15 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getTaskInitialState, taskReducer } from "./reducer/tasksReducer";
 
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-}
-
 export const TasksApp = () => {
   //const [todos, setTodos] = useState<Todo[]>([]);
   const [inputValue, setInputValue] = useState("");
 
-  const [state, dispatch] = useReducer(taskReducer, getTaskInitialState())
+  const [state, dispatch] = useReducer(taskReducer, getTaskInitialState());
+
+  useEffect(() =>{
+    localStorage.setItem('tasks-state', JSON.stringify(state));
+  }, [state])
 
   const addTodo = () => {
     if (inputValue.length === 0) return;
--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -1,3 +1,6 @@
+import { text } from "stream/consumers";
+import * as z from "zod";
+
 interface Todo {
     id: number;
     text: string;
@@ -15,16 +18,52 @@ interface TaskState {
 export type TaskAction =
     | { type: 'ADD_TODO', payload: string }
     | { type: 'TOGGLE_TODO', payload: number }
-    | { type: 'DELETE_TODO', payload: number }
+    | { type: 'DELETE_TODO', payload: number };
+
+
+const todoSchema = z.object({
+    id: z.number(),
+    text: z.string(),
+    completed: z.boolean(),
+});
+
+const TaskStateScheme = z.object({
+    todos: z.array(todoSchema),
+    length: z.number(),
+    completed: z.number(),
+    pending: z.number()
+})
 
 export const getTaskInitialState = (): TaskState => {
-    return{
-        todos: [],
-        completed: 0, 
-        pending: 0,
-        length: 0,
-    };
-}
+
+    const localStorageState = localStorage.getItem('tasks-state');
+
+    if (!localStorageState) {
+        return {
+            todos: [],
+            completed: 0,
+            pending: 0,
+            length: 0,
+        };
+    }
+
+    //validar con zod
+    const result = TaskStateScheme.safeParse(JSON.parse(localStorageState));
+
+    if(result.error){
+        console.log(result.error);
+        return {
+            todos: [],
+            completed: 0,
+            pending: 0,
+            length: 0,
+        };
+    }
+
+    return result.data;
+
+
+};
 
 
 export const taskReducer = (state: TaskState, action: TaskAction): TaskState => {
@@ -52,8 +91,8 @@ export const taskReducer = (state: TaskState, action: TaskAction): TaskState => 
                 ...state,
                 todos: currentTodos,
                 length: currentTodos.length,
-                completed: currentTodos.filter( todo => todo.completed).length,
-                pending: currentTodos.filter( todo => !todo.completed).length,
+                completed: currentTodos.filter(todo => todo.completed).length,
+                pending: currentTodos.filter(todo => !todo.completed).length,
 
             };
         }
@@ -68,8 +107,8 @@ export const taskReducer = (state: TaskState, action: TaskAction): TaskState => 
                 ...state,
                 todos: updatedTodos,
                 length: updatedTodos.length,
-                completed: updatedTodos.filter( todo => todo.completed).length,
-                pending: updatedTodos.filter( todo => !todo.completed).length,
+                completed: updatedTodos.filter(todo => todo.completed).length,
+                pending: updatedTodos.filter(todo => !todo.completed).length,
             };
         }
 
